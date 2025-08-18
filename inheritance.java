f2010186c1c35dd852e9cc8a37c1efe6ef43f521
Arti05-Ambre <ambrearti62@gmail.com>
2025-08-18 23:26:56 +0530

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,57 @@
+package com.inheritance;
+
+class Animal{
+	String Animalcolor = "White" ;
+	String Animalhabitat = " anywhere";
+	String Animalfood = " Omnivores";
+	void display() {
+		System.out.println("Color of animal is "+ Animalcolor);
+		System.out.println("Animals live in/on" + Animalhabitat);
+		System.out.println("Animal food is" + Animalfood);
+	}
+}
+	
+ class Aquatic extends Animal{
+	 void display0() {
+	 System.out.println("Animals that live in water");
+	 }
+ }
+ class Aerial extends Animal {
+	 void display1() {
+		 System.out.println("Animals that live in air");
+	 }
+	 
+ }
+  class Terrestrial extends Animal{
+	  void display2() {
+		  System.out.println("Animals that live on land");
+		  
+	  }
+	  
+  }
+  class Desert extends Animal{
+	  void display3() {
+		  System.out.println("Animals that live in desert");
+	  }
+  }
+  class Tiger extends Terrestrial{
+	  
+  }
+  
+  class Fish extends Aquatic{
+	  
+  }
+  class Eagle extends Aerial{
+	  
+  }
+  
+
+public class Demo {
+	public static void main (String[]  args) {
+		Tiger a1 = new Tiger();
+		a1.display2();
+		a1.display();
+	}
+}
+
+
